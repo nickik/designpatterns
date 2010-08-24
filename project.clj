15873d71bb379fc1dbe6a213fa208333aa3711b3
nickik <nickik@gmx.ch>
2010-08-24 21:49:34 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject titfortat "1.0.0-SNAPSHOT"
+  :description "Tit for Tat"
+  :dependencies [[org.clojure/clojure "1.2.0"] 
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-difftest "1.2.2"]])
