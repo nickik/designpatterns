2efb0b4cb648e0d81ae752cd3701ab7cace02d26
nickik <nickik@gmx.ch>
2010-12-01 18:49:58 +0100

    after pull

--- presentation/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-
-rm generated/ -r
-mkdir generated/
-cd generated/
-pdflatex ../DPP.tex
-xpdf DPP.pdf
--- src/clojure_dp/core.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure-dp.core)
-
-(defn TheCore []
-  "The Core is available")
